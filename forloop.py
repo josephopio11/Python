fe65a5a5cfba5893fbc7979e255479caf825741b
Joseph Opio <josephopio11@gmail.com>
2020-11-11 06:00:19 +0300

    master

---
+++ forloop.py
@@ -0,0 +1,15 @@
+# c = list(range(1,100))
+# # print(c)
+
+# total = 0
+
+# for num in c:
+#     if num % 3 == 0 and num % 5 == 0:
+#         print(num)
+#         total += num 
+
+# print(total)        
+
+for i in range(0,12):
+    for j in range(0,i):
+        print(i)
